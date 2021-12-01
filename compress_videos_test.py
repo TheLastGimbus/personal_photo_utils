cfb6c9626dd0f276bc5d422d182a8c190edced31
TheLastGimbus <mateusz.soszynski@tuta.io>
2021-12-02 00:49:36 +0100

    Add test for the *whole thing*

--- compress_videos_test.py
+++ compress_videos_test.py
@@ -20,3 +20,39 @@ def test_is_ignored():
     assert _ignored("video_2137.mp4") == True
     assert _ignored("video_2137.webm") == True
     assert _ignored("video_2137.mkv") == False
+
+
+def test_main():
+    import compress_videos as cv
+    from pathlib import Path
+
+    from __personal_photo_utils_test_data__.set_datetimes import set_datetimes
+    with open('__personal_photo_utils_test_data__/datetimes.json', 'r') as f:
+        import json
+        datetimes = json.load(f)
+        set_datetimes(datetimes, Path('__personal_photo_utils_test_data__/DCIM/Camera'))
+
+    assets = Path('./__personal_photo_utils_test_data__/DCIM/')
+    orig = assets / "orig"
+    orig.mkdir(exist_ok=True)
+
+    cv.INPUT_DIR = assets / "Camera"
+    cv.OUTPUT_DIR = cv.INPUT_DIR
+    cv.ORIGINALS_DIR = orig
+    cv.CMPIGNORE_FILE = cv.INPUT_DIR / ".cmpignore"
+
+    cv.main()
+
+    # That would mean .cmpignore is not working!
+    assert cv.stats['total_videos_found'] != cv.stats['total_videos_compressed']
+    # That would mean we are actually losing space!
+    assert cv.stats['uncompressed_space'] - cv.stats['compressed_space'] > 0
+
+    assert set(map(lambda x: x.name, cv.OUTPUT_DIR.glob('*'))) == {
+        'video-1533589738.cmp1.mp4', '.cmpignore', 'editing_multiple_lines_router_admin_pt2.webm',
+        'VID_20210407_014635.cmp1.mp4', 'VID_20191230_215442.mp4', '89122489_891195301331652_5070170126553186304_n.jpg',
+        'some_random_empty.txt', '2ANsayp.png'
+    }
+    assert set(map(lambda x: x.name, cv.ORIGINALS_DIR.glob('*'))) == {
+        'video-1533589738.mp4', 'VID_20210407_014635.mp4'
+    }
