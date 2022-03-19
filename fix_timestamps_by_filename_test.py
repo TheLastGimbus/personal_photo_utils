af37792d484a95e7dcedc76c146fe2e8c280f86c
TheLastGimbus <mateusz.soszynski@tuta.io>
2022-03-19 13:41:08 +0100

    Fucking timezones ;_;

--- fix_timestamps_by_filename_test.py
+++ fix_timestamps_by_filename_test.py
@@ -1,8 +1,10 @@
 def test_main():
     from unittest import mock
     import sys
+    from datetime import datetime
     with mock.patch.object(sys, 'argv', ['', 'signal', '-p', './__personal_photo_utils_test_data__/DCIM/Camera']):
         from pathlib import Path
         import fix_timestamps_by_filename as fix
         fix.main()
-        assert ((Path(fix.args.path) / 'signal-2022-03-12-203337_023.jpeg').stat().st_mtime == 1647113617)
+        assert ((Path(fix.args.path) / 'signal-2022-03-12-203337_023.jpeg').stat().st_mtime ==
+                datetime(2022, 3, 12, 20, 33, 37).timestamp())
